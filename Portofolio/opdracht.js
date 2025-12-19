8b4c2d35f66a1f1da21631a1870d5dfb5c1de11e
walidabdalbaki29-ui <walidabdalbaki29@gmail.com>
2025-12-19 09:04:36 +0100

    Week 4: Countdown toegevoegd en website geoptimaliseerd.

--- Portofolio/opdracht.js
+++ Portofolio/opdracht.js
@@ -1,3 +1,21 @@
+
+//data op opdracht.js
+var countDownDate = new Date("jul 29 , 2026 23:59:59").getTime();
+var x = setInterval(function () {
+  var now = new Date().getTime();
+  var distance = countDownDate - now;
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60 )) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  document.getElementById("countdown").innerHTML =
+    days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countdown").innerHTML = "EXPIRED";
+  }
+}, 1000);
+// HAMBURGER MENU
 const hamburger = document.getElementById("hamburger");
 const navMobile = document.getElementById("nav-mobile");
 
@@ -81,6 +99,5 @@ document.addEventListener("DOMContentLoaded", function () {
     greeting.style.marginBottom = "10px";
     greeting.style.fontFamily = "sans-serif";
   }
-
   updateGreeting();
 });
